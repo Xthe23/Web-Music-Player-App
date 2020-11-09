04e0a7bf9bc30017d15b193d9c26d5d7dd7ba299
Michael Kravitz <30937928+Xthe23@users.noreply.github.com>
2020-11-09 12:25:32 -0800

    Added State currSong, setIsPlaying, isPlaying to Player component
    Stylized with Scss using partials that are being imported to the main app scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
+
+import React, { useState } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currSong, setCurrSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currSong={currSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currSong={currSong}
+      />
     </div>
   );
 }
